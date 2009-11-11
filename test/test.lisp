6ca08038fa44a434fab5be4a5c0260903e0ed02c
Moskvitin Andrey <archimag@gmail.com>
2009-11-11 13:15:09 +0300

    added test for common-lisp-backend

--- test/test.lisp
+++ test/test.lisp
@@ -6,8 +6,7 @@
 ;;;; Author: Moskvitin Andrey <archimag@gmail.com>
 
 (defpackage #:closure-template.test
-  (:use #:cl #:iter #:lift)
-  (:import-from #:closure-template.parser #:parse-expression #:parse-template #:parse-single-template)
+  (:use #:cl #:iter #:lift #:closure-template)
   (:export #:run-closure-template-tests))
 
 (in-package #:closure-template.test)
@@ -94,6 +93,91 @@
   (ensure-same '(closure-template.parser:template ("test")
                  (closure-template.parser:for-tag ((:variable "x") (:range 4 10 2)) " ! "))
                (parse-single-template "{template test}{for $x in range(4, 10, 2)} ! {/for}{/template}")))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; common-lisp-backend test
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftestsuite common-lisp-backend-test (closure-template-test) ()
+  (:run-setup :once-per-test-case )
+  (:dynamic-variables *default-translate-package*)
+  (:setup (setf *default-translate-package*
+                (make-template-package :closute-template.test.templates)))
+  (:teardown (delete-package :closute-template.test.templates)))
+
+(addtest (common-lisp-backend-test)
+  hello-world
+  (ensure-same "Hello world"
+               (progn
+                (compile-template :common-lisp-backend
+                                  "{template hello-world}Hello world{/template}")
+                (funcall (find-symbol "HELLO-WORLD" :closute-template.test.templates)))))
+            
+(addtest (common-lisp-backend-test)
+  hello-name
+  (ensure-same "Hello Closure Template"
+               (progn
+                (compile-template :common-lisp-backend
+                                  "{template hello-name}Hello {$name}{/template}")
+                (funcall (find-symbol "HELLO-NAME" :closute-template.test.templates)
+                         '(:name "Closure Template")))))
+
+(addtest (common-lisp-backend-test)
+  dotted-vars-1
+  (ensure-same "Hello world"
+               (progn
+                (compile-template :common-lisp-backend
+                                  "{template dotted}{$obj.first} {$obj.second}{/template}")
+                (funcall (find-symbol "DOTTED" :closute-template.test.templates)
+                         '(:obj (:first "Hello" :second "world"))))))
+               
+
+(addtest (common-lisp-backend-test)
+  foreach-1
+  (ensure-same " alpha beta gamma"
+               (progn
+                (compile-template :common-lisp-backend
+                                  "{template test}{foreach $opernand in $opernands} {$opernand}{/foreach}{/template}")
+                (funcall (find-symbol "TEST" :closute-template.test.templates)
+                         '(:opernands ("alpha" "beta" "gamma"))))))
+
+(addtest (common-lisp-backend-test)
+  foreach-2
+  (ensure-same '(" alpha beta gamma"  "Hello world")
+               (progn
+                (compile-template :common-lisp-backend
+                                  "{template test}{foreach $opernand in $opernands} {$opernand}{ifempty}Hello world{/foreach}{/template}")
+                (list (funcall (find-symbol "TEST" :closute-template.test.templates)
+                               '(:opernands ("alpha" "beta" "gamma")))
+                      (funcall (find-symbol "TEST" :closute-template.test.templates)
+                               nil)))))
+
+(addtest (common-lisp-backend-test)
+  for-1
+  (ensure-same " 0 1 2 3 4"
+               (progn
+                (compile-template :common-lisp-backend
+                                  "{template test}{for $i in range(5)} {$i}{/for}{/template}")
+                (funcall (find-symbol "TEST" :closute-template.test.templates)))))
+
+(addtest (common-lisp-backend-test)
+  for-2
+  (ensure-same "456789"
+               (progn
+                (compile-template :common-lisp-backend
+                                  "{template test}{for $i in range(4, 10)}{$i}{/for}{/template}")
+                (funcall (find-symbol "TEST" :closute-template.test.templates)))))
+
+(addtest (common-lisp-backend-test)
+  for-3
+  (ensure-same "147"
+               (progn
+                (compile-template :common-lisp-backend
+                                  "{template test}{for $i in range($from, $to, $by)}{$i}{/for}{/template}")
+                (funcall (find-symbol "TEST" :closute-template.test.templates)
+                         '(:from 1 :to 10 :by 3)))))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Run all tests

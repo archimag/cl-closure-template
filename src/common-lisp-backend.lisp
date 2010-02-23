archimag/cl-closure-template

commit a53b7e8e3cfaf42a744ed348c83824ace3e8aa62

common lisp backend: convert namespace and template names from camelCase to lisp-hyphenated-style, as it is already done with variable substitution.

src/common-lisp-backend.lisp
@@ -44,7 +44,7 @@
 (defun escape-uri-component (str)
   (escape-string str "~!*()'"))
 
-(defun make-template-package (&optional (name "CLOSURE-TEMPLATE.SHARE") &aux (upname (string-upcase name)))
+(defun make-template-package (&optional (name "CLOSURE-TEMPLATE.SHARE") &aux (upname (lispify-string name)))
   (or (find-package upname)
       (eval `(defpackage ,(if name
                               upname
@@ -112,7 +112,7 @@
                        (make-template-package (car args))
                        *default-translate-package*)))
     (iter (for tmpl in (cdr args))
-          (let ((symbol (intern (string-upcase (car (second tmpl))))))
+          (let ((symbol (intern (lispify-string (car (second tmpl))))))
             (export symbol)
             (proclaim (list 'ftype
                             'function
@@ -132,7 +132,7 @@
          (binds (iter (for var in *template-variables*)
                       (collect (list (find-symbol (symbol-name var) *package*)
                                      `(getf $data$ ,var))))))
-    `(defun ,(intern (string-upcase (caar args))) (,@(unless binds '(&optional)) $data$)
+    `(defun ,(intern (lispify-string (caar args))) (,@(unless binds '(&optional)) $data$)
        (let ((*loops-vars* nil) ,@binds)
          (macrolet ((write-template-string (str)
 		      (let ((g-str (gensym)))
@@ -227,7 +227,7 @@
 
 
 (defmethod translate-named-item ((backend common-lisp-backend) (item (eql 'closure-template.parser:call)) args)
-  (let ((fun-name (or (find-symbol (string-upcase (first args)))
+  (let ((fun-name (or (find-symbol (lispify-string (first args)))
                       (error "Unknow template ~A" (first args)))))
     `(let ((data ,(cond
                    ((eql (second args) :all) '$data$)

src/expression.lisp
@@ -39,13 +39,15 @@
     "min" "max"
     "range"))
 
+(defun lispify-string (str)
+  (coerce (iter (for ch in-string str)
+		(when (upper-case-p ch)
+		  (collect #\-))
+		(collect (char-upcase ch)))
+	  'string))
+
 (defun lispify-name (str)
-  (intern (coerce (iter (for ch in-string str)
-                        (when (upper-case-p ch)
-                          (collect #\-))
-                        (collect (char-upcase ch)))
-                  'string)
-          :keyword))
+  (intern (lispify-string str) :keyword))
 
 (defun make-expression-symbol (string)
   "Convert string to symbol, preserving case, except for AND/OR/NOT/FORALL/EXISTS."

src/packages.lisp
@@ -10,7 +10,8 @@
   (:use #:cl #:iter)
   (:export #:parse-expression
            #:bad-expression-condition
-           #:not-equal))
+           #:not-equal
+	   #:lispify-string))
 
 (defpackage #:closure-template.parser
   (:use #:cl #:iter #:closure-template.parser.expression)
@@ -20,7 +21,7 @@
 (defpackage #:closure-template
   (:use #:cl #:iter)
   (:import-from #:closure-template.parser #:parse-template)
-  (:import-from #:closure-template.parser.expression #:parse-expression #:not-equal)
+  (:import-from #:closure-template.parser.expression #:parse-expression #:not-equal #:lispify-string)
   (:export #:parse-template
            #:parse-expression
            #:translate-template

commit 4bd93a7f48ea835ce9c093ddfbb4ba920f22c8d2

Fixed regression introduced by commit a53b7e8e3cfaf42a744ed348c83824ace3e8aa62

src/common-lisp-backend.lisp
@@ -101,14 +101,14 @@
 (defun escape-uri-component (str)
   (escape-string str "~!*()'"))
 
-(defun make-template-package (&optional (name "CLOSURE-TEMPLATE.SHARE") &aux (upname (lispify-string name)))
-  (or (find-package upname)
-      (eval `(defpackage ,(if name
-                              upname
-                              "CLOSURE-TEMPLATE.SHARE")
-               (:use #:cl)
-               (:import-from #:closure-template #:*template-output*)))))
-
+(defun make-template-package (&optional (name "closure-template.share"))
+  (let ((lispified-name (if (stringp name)
+                            (lispify-string name)
+                            name)))
+    (or (find-package lispified-name)
+        (eval `(defpackage ,lispified-name
+                 (:use #:cl)
+                 (:import-from #:closure-template #:*template-output*))))))
 
 (defparameter *default-translate-package*
   (make-template-package))
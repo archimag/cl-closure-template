b03444a43f0a92cecf6f9e95a9fd041acfad47a4
Andrey Kutejko <andy128k@gmail.com>
2012-12-10 12:38:56 +0200

    Fallback syntax for object accessor.
    IE doesn't like expressions with reserved keywords
    e. g. $env$.new
    
    List of words is taken from Mozilla and MS sites
    
    https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Reserved_Words
    http://msdn.microsoft.com/en-us/library/ttyab5c8.aspx

--- src/javascript-backend.lisp
+++ src/javascript-backend.lisp
@@ -100,11 +100,31 @@
 
 ;;;; variable
 
+(defparameter +reserved-js-words+ '("abstract" "assert" "boolean" "break"
+                                    "byte" "case" "catch" "char" "class"
+                                    "const" "continue" "debugger" "decimal"
+                                    "default" "delete" "do" "double" "else"
+                                    "ensure" "enum" "event" "export" "extends"
+                                    "false" "final" "finally" "float" "for"
+                                    "function" "get" "goto" "if" "implements"
+                                    "import" "in" "instanceof" "int"
+                                    "interface" "internal" "invariant" "let"
+                                    "long" "namespace" "native" "new" "null"
+                                    "package" "private" "protected" "public"
+                                    "require" "return" "sbyte" "set" "short"
+                                    "static" "super" "switch" "synchronized"
+                                    "this" "throw" "throws" "transient" "true"
+                                    "try" "typeof" "uint" "ulong" "use"
+                                    "ushort" "var" "void" "volatile" "while"
+                                    "with" "yield"))
+
 (defmethod write-expression ((expr var) out)
   (let ((name (var-jsname expr)))
-    (unless (member name  *local-variables* :test #'string=)
-      (write-string "$env$." out))
-    (write-string name out)))
+    (if (member name  *local-variables* :test #'string=)
+        (write-string name out)
+        (if (member name +reserved-js-words+ :test #'string=)
+            (format out "$env$['~A']" name)
+            (format out "$env$.~A" name)))))
 
 ;;;; fcall
 

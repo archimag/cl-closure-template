da15e47edb8407f90e601fc6ef2197227cc5308d
Alexey Martynov <alexey_martynov@rbcmail.ru>
2013-01-05 19:40:40 +0400

    Fix warning about implicitly defined generic function "compile-template"

---
+++ src/interface.lisp
@@ -0,0 +1,11 @@
+;;;; common-lisp-backend.lisp
+;;;;
+;;;; This file is part of the cl-closure-template library, released under Lisp-LGPL.
+;;;; See file COPYING for details.
+;;;;
+;;;; Author: Moskvitin Andrey <archimag@gmail.com>
+
+(in-package #:closure-template)
+
+(defgeneric compile-template (backend template)
+  (:documentation "Compile templates from TEMPLATE using BACKEND as target language"))
